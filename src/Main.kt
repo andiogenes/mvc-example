df64bec7addc3882d1fcf78d3ba3f2070b6bd51c
LAPTOP-VACUB08O\Антон <megadeathlightsaber@gmail.com>
2019-11-20 17:06:45 +0700

    Entry point reduction and some exports for JS interop.

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,19 @@
 import app.App
 
 // Задана многозвенная ломаная. Проверить, будет ли она самопересекающейся.
-fun main() {
-    val app = App()
+internal val app = App()
+
+@JsName("addPoint")
+fun addPoint(x: Int, y: Int) {
+    app.addPoint(x, y)
+}
+
+@JsName("movePoint")
+fun movePoint(x1: Int, y1: Int, x2: Int, y2: Int) {
+    app.movePoint(x1, y1, x2, y2)
+}
+
+@JsName("removePoint")
+fun removePoint(x: Int, y: Int) {
+    app.removePoint(x, y)
 }
